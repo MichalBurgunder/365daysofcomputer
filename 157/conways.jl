3ff9420c58b05f29e00cfacd3406b2791340cf9b
Michal Burgunder <michal_burgunder@yahoo.com>
2025-01-30 13:27:29 +0100

    working conways

--- 157/conways.jl
+++ 157/conways.jl
@@ -21,25 +21,32 @@ using FFMPEG
 #     rm(images_directory, recursive=true)
 # end
 
-function check_valid(matrix, i, j, offset)
+function check_valid(matrix, i, j)
     x,y = size(matrix)
-    return (i < 1 | i < x | j < 1 |j < y) ? 0 : 1
+    return (i < 1 || i > x || j < 1 || j > y) ? 0 : matrix[i,j]
 end
 
 cell_offset = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,1], [1,0], [1,-1]]
 
 function rule(matrix, i, j)
-    the_sum = sum([check_valid(matrix, i, j, offset) for offset in cell_offset])
-    return the_sum == 2 || the_sum == 3 ? 1 : 0
+    the_sum = sum([check_valid(matrix, i+os[1], j+os[2]) for os in cell_offset])
+    if matrix[i,j] == 1
+        return (the_sum == 2 || the_sum == 3) ? 1 : 0
+    else
+        return the_sum == 3 ? 1 : 0
+    end
+        
 end
 
 function single_timestep(matrix, the_size)
+    new_matrix = falses(the_size, the_size)
     for i in range(1, the_size)
         for j in range(1, the_size)
-            matrix[i,j] = rule(matrix, i, j)
+            new_matrix[i,j] = rule(matrix, i, j)
         end
     end
-    return matrix
+
+    return new_matrix
 end
 
 
@@ -54,23 +61,20 @@ end
 
 images_directory = "/Users/michal/Documents/365daysofcomputer/365daysofcomputer-code/157/images"
 file_name = "conway"
+
 function gif_creation(steps)
     images = []
 
-
     for i in 1:steps
         ending = lpad(i, 4, "0")
-        image = load("$images_directory/$file_name-$ending.png")
-        # rgb_matrix = convert(Matrix{RGB}, image)
-        # final_image = map(clamp01nan, rgb_matrix)
+        image = load("$images_directory/$ending.png")
         push!(images, image)
     end
 
     # create the gif
-   
     framerate = 100
     gifname = "/Users/michal/Documents/365daysofcomputer/365daysofcomputer-code/157/conways_glider.gif"
-    FFMPEG.ffmpeg_exe(`-framerate $(framerate) -f image2 -i $(images_directory)/conway-0001.png -y $(gifname)`)
+    FFMPEG.ffmpeg_exe(`-framerate $(framerate) -f image2 -i $(images_directory)/%04d.png -y $(gifname)`)
 
     # we remove the images, as we now have our gif
     # rm(images_directory, recursive=true)
@@ -84,24 +88,46 @@ function save_image(maze)
     name = lpad(counter, 4, "0")
     counter += 1
 
-    save("$images_directory/$file_name-$name.png", maze)
+    save("$images_directory/$name.png", maze)
 end
 
 
 
-function conways_game_of_life(the_size=10, steps=100)
+function shave_off_squares(matrix, squares=10)
+    n, m = size(matrix)
+    new_matrix = falses(n-squares, n-squares)
+
+    for i in 1:n-2*squares
+        for j in 1:m-2*squares
+            new_matrix[i,j] = matrix[i+squares, j+squares]
+        end
+    end
+
+    return new_matrix
+end
+
+function conways_game_of_life(the_size=100, steps=100)
     matrix = falses(the_size, the_size)
 
     insert_object(matrix, (Int(the_size/2),Int(the_size/2)), [[-1,0], [1,0], [1,1], [1,-1], [0,1]])
-    matrix[1,1] = 1
-    println(matrix[1,1])
-    save_image(matrix)
+    # matrix[1,1] = 1
 
-    all_matricies = []
+    # println(matrix)
+    # println(matrix[1,1])
+    # exit()
+    mat_to_save = shave_off_squares(matrix)
+    save_image(mat_to_save)
+
     for i in 1:steps
-        matrix = single_timestep(matrix, the_size)
-        save_image(matrix)
+        new_matrix = single_timestep(matrix, the_size)
+        # println(new_matrix)
+        # exit()
+        mat_to_save2 = shave_off_squares(matrix)
+        save_image(mat_to_save2)
+        matrix = new_matrix
     end
+
+
 
     # final_path = 
 
