3dc20c3752bb03b0e6a419448560886c716083bf
Michal Burgunder <michal_burgunder@yahoo.com>
2025-02-04 11:33:44 +0100

    test process for RAM image

---
+++ 69/test_process.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+  cout << "This is data written directly into RAM" << endl;
+
+ while (true) {
+    int i = 9;
+
+ }
+
+  cout << "...done! Exiting";
+  return 0;
+}
