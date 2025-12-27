8a1c2ddadc62ce739bb2d095d7468d28e6ffc961
Michal Burgunder <michal_burgunder@yahoo.com>
2025-12-27 09:10:47 +0100

    syntax highlighting class file

---
+++ 222/SampleClass.java
@@ -0,0 +1,20 @@
+class SampleClass {
+    public static int main(String[] args) {
+       for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                if(args[0] == 'hello') {
+                    return 0;
+                } else {
+                    return 2;
+                }
+                
+            }
+        }
+    }
+}
+
+
+class SampleClass {public static int main(String[] args) {for(int i = 0; i < 10;
+i++){for(int j = 0; j < 10; j++) {if(args[0] == 'hello') {return 0;} else {
+return 2;}}}}}
+
