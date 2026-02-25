9bba6fb5eba14da8b8f3b7f57dbda19322f45337
Michal Burgunder <michal_burgunder@yahoo.com>
2026-02-25 10:20:36 +0100

    thrown ball cleanup

--- 355/wolves_and_rabbits.py
+++ 355/wolves_and_rabbits.py
@@ -1,9 +1,15 @@
+# WOLVES AND RABBITS
+
+# This very basic simulation demonstrates certain problems of simulating certain processes in the real world. 
 
 
-# r = 1000
-# w = 4
+# 1. Reducing animal numbers below zero
+# By creating a straighforward simulation of wolves eating rabbits and rabbits multiplying, the problem of having a number reduced to below zero shows that a simulation is faulty.
 
-# rra = 1.2
+# r = 1000 # number of rabbits 
+# w = 6 # number of wolves
+
+# rra = 200
 # wra = 1.1
 
 # rre = w*10
@@ -11,33 +17,33 @@
 
 # for i in range(0, 20):
 #     # print()
-#     wre = (0 if (r/w) > 10 else w-r/w**2)
 #     # print(wre)
-#     r = r*rra - rre*w
-#     w = w*wra - wre
+#     r = r+rra - rre*w
+#     w = w*wra
     
-#     print(r, w)
-#     # exit()/
+#     print(round(r,2), round(w, 2))
 
 
 
+# 2. Achieving Semistable State 
+# When we assume that wolf and rabbit populations always have a certain amout of them in the world, then each can multiply again to accomodate the other species. When we assume that there are always 2000 rabbits around that the wolves cannot eat (e.g. they hide very well) and 59 wolves that do not die (because they have other food sources) we can simualte a stable state, where both species can coexist. 
 
-r = 1000
-w = 6
+r = 1000 # number of rabbits 
+w = 4 # number of wolves
+always_r = 2000 # always existing rabbits
+wolves_r = 100 # always existing wolves
 
-rra = 200
-wra = 1.1
+rra = 1.2 # rate at which rabbits multiply
+wra = 1.1 # rate at which wolves multiply
 
 rre = w*10
 wre = 0 if r / w < 10 else w*(r / w)
 
 for i in range(0, 20):
-    # print()
-    # print(wre)
-    r = r+rra - rre*w
-    w = w*wra
-    
-    print(round(r,2), round(w, 2))
-    # exit()/
+    wre = (0 if (r/w) > 10 else w-r/w**2)
 
+    r = r*rra - rre*w
+    w = w*wra - wre
+
+    print(r + always_r, w + wolves_r)
 
